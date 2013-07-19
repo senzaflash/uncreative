fc6386f37852dfc0ede2699cd9c3cfcf32f02b6c
Arthur Wang <arthurw1989@gmail.com>
2013-07-19 18:40:37 -0400

    set up heroku memcachier properly

--- uncreative/settings.py
+++ uncreative/settings.py
@@ -190,3 +190,17 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+
+os.environ['MEMCACHE_SERVERS'] = os.environ.get('MEMCACHIER_SERVERS', '').replace(',', ';')
+os.environ['MEMCACHE_USERNAME'] = os.environ.get('MEMCACHIER_USERNAME', '')
+os.environ['MEMCACHE_PASSWORD'] = os.environ.get('MEMCACHIER_PASSWORD', '')
+
+CACHES = {
+  'default': {
+    'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
+    'TIMEOUT': 500,
+    'BINARY': True,
+    'OPTIONS': { 'tcp_nodelay': True }
+  }
+}
