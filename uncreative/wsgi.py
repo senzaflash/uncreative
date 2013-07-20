a2599463c4e5ad7c4f9712542300e126364b976b
Arthur Wang <arthurw1989@gmail.com>
2013-07-20 17:48:32 -0400

    production settings

--- uncreative/wsgi.py
+++ uncreative/wsgi.py
@@ -20,17 +20,16 @@ import os
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "uncreative.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "uncreative.settings")
-
+'''
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
+'''
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
-
 
 
 #for production
